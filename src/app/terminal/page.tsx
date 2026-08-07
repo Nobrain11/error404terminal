a40355766e4030dd3c918163f903e65f522294f4
Nobrain11 <chainpulseink@gmail.com>
2026-08-07 02:54:52 +0100

    Add TerminalPage component with dynamic import

---
+++ src/app/terminal/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import dynamic from "next/dynamic";
+
+const Terminal = dynamic(() => import("@/components/Terminal"), { ssr: false });
+
+export default function TerminalPage() {
+  return <Terminal />;
+}
